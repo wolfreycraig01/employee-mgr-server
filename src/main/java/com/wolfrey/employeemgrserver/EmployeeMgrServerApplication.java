87ce7b03695356963ff977c7776583139d25798a
wolfrey01 <wolfreycraig@gmail.com>
2022-07-16 09:51:18 -0400

    first push

---
+++ src/main/java/com/wolfrey/employeemgrserver/EmployeeMgrServerApplication.java
@@ -0,0 +1,13 @@
+package com.wolfrey.employeemgrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMgrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMgrServerApplication.class, args);
+	}
+
+}
